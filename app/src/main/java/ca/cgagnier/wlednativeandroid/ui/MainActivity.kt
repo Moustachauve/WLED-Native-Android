779a5d52920644bac8e317465b881feb17a2f406
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2024-10-11 23:56:56 -0400

    Move the mainActivity

--- app/src/main/java/ca/cgagnier/wlednativeandroid/ui/MainActivity.kt
+++ app/src/main/java/ca/cgagnier/wlednativeandroid/ui/MainActivity.kt
@@ -1,11 +1,10 @@
-package ca.cgagnier.wlednativeandroid
+package ca.cgagnier.wlednativeandroid.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import ca.cgagnier.wlednativeandroid.ui.MainNavHost
 import ca.cgagnier.wlednativeandroid.ui.theme.WLEDNativeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
