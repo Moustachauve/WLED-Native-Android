d32b43d5ee14f197625792ba9bb55cd829ba1e18
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2024-11-06 02:52:15 -0500

    Bump version code to 38

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "ca.cgagnier.wlednativeandroid"
         minSdk = 24
         targetSdk = 35
-        versionCode = 37
+        versionCode = 38
         versionName = "5.0.0"
         resourceConfigurations += listOf("en", "fr")
 
