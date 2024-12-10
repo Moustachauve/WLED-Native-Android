088bb3ac5dd18aaa3f182ff72bbcd2eaa58aea43
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2024-12-09 22:26:12 -0500

    Fix battery icon showing for devices with any user mods

--- app/src/main/java/ca/cgagnier/wlednativeandroid/service/device/api/JsonApiRequestHandler.kt
+++ app/src/main/java/ca/cgagnier/wlednativeandroid/service/device/api/JsonApiRequestHandler.kt
@@ -152,7 +152,7 @@ class JsonApiRequestHandler @Inject constructor(
             productName = deviceStateInfo.info.product ?: Device.UNKNOWN_VALUE,
             release = deviceStateInfo.info.release ?: Device.UNKNOWN_VALUE,
             batteryPercentage = deviceStateInfo.info.userMods?.batteryLevel?.get(0) as? Double ?: 0.0,
-            hasBattery = (deviceStateInfo.info.userMods != null),
+            hasBattery = (deviceStateInfo.info.userMods?.batteryLevel != null),
         )
 
         if (saveChanges && updatedDevice != device) {
